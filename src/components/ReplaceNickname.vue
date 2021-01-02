7bf6fdc314b0859f678f97a7bc52440dfd89962e
Akiko97 <296339417@163.com>
2021-01-02 11:59:15 +0800

    first commit

---
+++ src/components/ReplaceNickname.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="replacenickname">
+    <h1>微信昵称转换</h1>
+    <el-input
+      type="textarea"
+      :autosize="{ minRows: 5, maxRows: 99}"
+      placeholder="请输入内容"
+      v-model="input_string">
+    </el-input>
+    <div style="margin: 20px 0;"></div>
+    <el-button
+      type="primary"
+      icon="el-icon-refresh"
+      v-on:click="transform">转换
+    </el-button>
+    <div style="margin: 20px 0;"></div>
+    <el-input
+      type="textarea"
+      :autosize="{ minRows: 5, maxRows: 99}"
+      placeholder="转换结果"
+      v-model="output_string">
+    </el-input>
+    <div style="margin: 50px 0;"></div>
+    <h6 style="color: #9a9a9a;">@内部使用</h6>
+  </div>
+</template>
+
+<script>
+import {nameList} from "../data/NameList.js"
+export default {
+  name: 'ReplaceNickname',
+  data() {
+    return {
+      input_string: '',
+      output_string: '',
+      list: nameList,
+      length: nameList.length
+    }
+  },
+  methods: {
+    transform() {
+      var temp = this.input_string;
+      for (var i = 0; i < this.length; i++) {
+        temp = temp.replace(this.list[i].name, this.list[i].nickname);
+      }
+      temp = temp.replace("、", " ");
+      if (temp[temp.length - 1] != " ") {
+        temp += " ";
+      }
+      this.output_string = temp;
+    }
+  }
+}
+</script>
---
+++ src/data/NameList.js
@@ -0,0 +1,16 @@
+var heart = "❤️"
+export var nameList = [
+  {name: "宣传文化部", nickname: "@毕诗豪－宣传文化部"},
+  {name: "营商合作局", nickname: "@乌托邦"},
+  {name: "科技创新局", nickname: "@科技创新局 徐丞"},
+  {name: "商务金融局", nickname: "@赵齐"},
+  {name: "社会事业局", nickname: "@事业局-丁建海"},
+  {name: "开发建设局", nickname: "@庆一年"},
+  {name: "城市运行局", nickname: "@" + heart + "颖🐶"},
+  {name: "党群服务中心", nickname: "@王继红-党群服务中心"},
+  {name: "综合执法", nickname: "@春和景颐"},
+  {name: "荣华街道", nickname: "@飞"},
+  {name: "博兴街道", nickname: "@博兴街道9小工作组"},
+  {name: "亦庄控股", nickname: "@亦庄控股 翟炳衡"},
+  {name: "亦庄国投", nickname: "@亦庄国投吴泽波"},
+];
